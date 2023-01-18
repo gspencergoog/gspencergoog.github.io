be7f7ca30e577448b77cbf097898085cf94857f3
Greg Spencer <gspencer@google.com>
2023-01-18 09:52:34 -0800

    re-enable tree shaking

--- counter_example/publish.sh
+++ counter_example/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flutter build web --release --no-tree-shake-icons --base-href="/counter_example/publish/" --output=publish
+flutter build web --release "$@" --base-href="/counter_example/publish/" --output=publish
